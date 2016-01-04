76a4d62fd59605a49081c2a7cc22ff4b7a4fa295
Liam Staskawicz <liam@stask.net>
2016-01-04 10:36:30 -0800

    sub: ability to create user subscription

--- rtps/sub.go
+++ rtps/sub.go
@@ -1,6 +1,8 @@
 package rtps
 
-import ()
+import (
+	"fmt"
+)
 
 type dataCallback func(r *receiver, submsg *subMsg, scheme uint16, b []byte)
 type msgCallback func(b []byte)
@@ -22,4 +24,17 @@ type Sub struct {
 	dataCB    dataCallback
 	msgCB     msgCallback
 	reliable  bool
+}
+
+func AddUserSub(topicName, typeName string, cb msgCallback) {
+	subEID := createUserID(ENTITYID_KIND_READER_NO_KEY)
+	fmt.Printf("frudp_add_user_sub(%s, %s) on EID 0x%08x\r\n", topicName, typeName, subEID)
+	defaultSession.addSub(&Sub{
+		topicName: topicName,
+		typeName:  typeName,
+		readerEID: subEID,
+		msgCB:     cb,
+		reliable:  false,
+	})
+	//sedp_publish_sub(&sub); // can't do this yet; spdp hasn't started bcast
 }
